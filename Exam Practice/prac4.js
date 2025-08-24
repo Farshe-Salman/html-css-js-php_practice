5a02532cad22dbbec0f0c4a36c4701e72a9cdfc0
Farshe-Salman <salmanfarshe3071@gmail.com>
2025-08-24 08:55:13 +0600

    add some new modify

---
+++ Exam Practice/prac4.js
@@ -0,0 +1,7 @@
+let student=["salman", "miyko", "mim"];
+
+console.log("Student name: ", student);
+
+console.log("Student name: ", student.toString());
+
+console.log("Student name: ", student.join(' $ '));
